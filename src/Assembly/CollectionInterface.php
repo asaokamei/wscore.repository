eb7e094a5bcb165e85166252147869578220ec63
Asao <kamei@workspot.jp>
2016-11-19 14:20:17 +0900

    test Collection. add comments in CollectionInterface.

--- src/Assembly/CollectionInterface.php
+++ src/Assembly/CollectionInterface.php
@@ -12,29 +12,115 @@ use WScore\Repository\Entity\EntityInterface;
 interface CollectionInterface extends \IteratorAggregate, \ArrayAccess, \Countable
 {
     /**
+     * sets entities for collection.
+     * 
      * @param EntityInterface[] $entities
      */
     public function setEntities($entities);
 
     /**
+     * executes SQL statement and stores the result entities.
+     * 
      * @param string $sql
      * @param array  $data
      */
     public function execute($sql, $data = []);
 
     /**
+     * finds entities for collection. 
+     * 
      * @param array $key
      */
     public function find(array $key);
 
     /**
+     * find an entity by key value. 
+     * 
      * @param array|string $key
      */
     public function findByKey($key);
 
     /**
+     * saves entities to database. 
+     */
+    public function save();
+    
+    /**
+     * loads related entities with $name. 
+     * the repository must implement a method $repo->$name 
+     * that returns RelationInterface object.
+     * 
      * @param string $name
      * @return CollectRelatedInterface
      */
     public function load($name);
+
+    /**
+     * returns a new collection with entities filtered out
+     * by $callable returning false. 
+     * $callable = function(EntityInterface): bool
+     * 
+     * @param callable $callable
+     * @return CollectionInterface
+     */
+    public function filter(callable $callable);
+
+    /**
+     * applies $callable for all entities.
+     * $callable = function(EntityInterface): void
+     * 
+     * @param callable $callable
+     * @return $this
+     */
+    public function walk(callable $callable);
+
+    /**
+     * creates an array from return value of $callable.
+     * $callable = function(EntityInterface): mixed
+     * 
+     * @param callable $callable
+     * @return array
+     */
+    public function map(callable $callable);
+
+    /**
+     * returns an array consisted of entity's column value. 
+     * 
+     * @param string $column
+     * @return array
+     */
+    public function column($column);
+
+    /**
+     * reduces into value. 
+     * 
+     * @param callable $callable
+     * @param null|mixed $initial
+     * @return mixed|EntityInterface
+     */
+    public function reduce(callable $callable, $initial = null);
+
+    /**
+     * sums the $column value. value must be numeric. 
+     * 
+     * @param string $column
+     * @return int
+     */
+    public function sum($column);
+
+    /**
+     * returns max value of the $column value. value must be numeric.
+     * 
+     * @param string $column
+     * @return int|mixed
+     */
+    public function max($column);
+
+    /**
+     * returns min value of the $column value. value must be numeric.
+     * 
+     * @param string $column
+     * @return int|mixed
+     */
+    public function min($column);
 }
---
+++ tests/Repository/Assembly/CollectionTest.php
@@ -0,0 +1,158 @@
+<?php
+namespace tests\Repository\Assembly;
+
+use Interop\Container\ContainerInterface;
+use PDO;
+use tests\Utils\Container;
+use WScore\Repository\Assembly\CollectHasSome;
+use WScore\Repository\Assembly\Collection;
+use WScore\Repository\Assembly\CollectionInterface;
+use WScore\Repository\Assembly\CollectJoin;
+use WScore\Repository\Entity\EntityInterface;
+use WScore\Repository\Query\PdoQuery;
+use WScore\Repository\Relations\Join;
+use WScore\Repository\Repo;
+use WScore\Repository\Repository\Repository;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $c;
+
+    /**
+     * @var CollectionInterface
+     */
+    private $users;
+
+    function setup()
+    {
+        class_exists(Container::class);
+        class_exists(Repo::class);
+        class_exists(Repository::class);
+        class_exists(PdoQuery::class);
+        class_exists(Join::class);
+        class_exists(CollectHasSome::class);
+        class_exists(CollectJoin::class);
+
+        $this->c = $this->getFullContainer();
+        $this->setDb($this->c->get(PDO::class));
+        $this->users = $this->c->get('u');
+        $this->users->find([]);
+    }
+
+    /**
+     * @return Container
+     */
+    function getFullContainer()
+    {
+        $c = new Container();
+        $c->set(PDO::class, function () {
+            $pdo = new PDO('sqlite::memory:');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $pdo;
+        });
+        $c->set(Repo::class, function (ContainerInterface $c) {
+            $repo = new Repo($c);
+            return $repo;
+        });
+        $c->set('u', function(ContainerInterface $c) {
+            $users = new Collection($c->get(Repo::class)->getRepository('users', ['user_id']));
+            return $users;
+        });
+
+        return $c;
+    }
+
+    function setDb(PDO $pdo)
+    {
+        $create /** @lang SQLite */
+            = <<<SQL
+CREATE TABLE users (
+    user_id     INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        VARCHAR(64) NOT NULL,
+    gender      INTEGER,
+    score       INTEGER
+);
+SQL;
+        $pdo->exec($create);
+
+        $insert = <<<SQL
+INSERT INTO users (user_id, name, gender, score) VALUES (?, ?, ?, ?);
+SQL;
+        $list   = [
+            [1, 'test', 'male', 10],
+            [2, 'more', 'female', 20],
+            [3, 'done', 'male', 30],
+            [4, 'test', 'female', 40],
+        ];
+        $stmt   = $pdo->prepare($insert);
+        foreach ($list as $rec) {
+            $stmt->execute($rec);
+        }
+    }
+
+    /**
+     * @test
+     */
+    function count_returns_number_of_entities()
+    {
+        $users = $this->users;
+        $this->assertEquals(4, $users->count());
+    }
+
+    /**
+     * @test
+     */
+    function filter_creates_subset_of_new_collection()
+    {
+        $users = $this->users;
+        $females = $users->filter(function($entity) {
+            return $entity->gender == 'female';
+        });
+        $this->assertEquals(4, $users->count());
+        $this->assertEquals(2, $females->count());
+    }
+
+    /**
+     * @test
+     */
+    function walk_alters_all_entities()
+    {
+        $users = $this->users;
+        $users->walk(function (EntityInterface $entity) {
+            $entity->fill(['score' => 100]);
+        });
+        foreach($users as $user) {
+            $this->assertEquals(100, $user->score);
+        }
+    }
+
+    /**
+     * @test
+     */
+    function map_will_extract_array()
+    {
+        $users = $this->users;
+        $scores = $users->map(function (EntityInterface $entity) {
+            return $entity->get('score');
+        });
+        $this->assertEquals([10, 20, 30, 40], $scores);
+        
+        $scores = $users->column('score');
+        $this->assertEquals([10, 20, 30, 40], $scores);
+    }
+
+    /**
+     * @test
+     */
+    function sum_max_min_return_some_int()
+    {
+        $users = $this->users;
+        $this->assertEquals(100, $users->sum('score'));
+        $this->assertEquals(40, $users->max('score'));
+        $this->assertEquals(10, $users->min('score'));
+    }
+}
